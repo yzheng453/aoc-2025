3a8eb1abcc25968ecc0534f9fe0269d1051b674e
yz <4067126+yzheng453@users.noreply.github.com>
2025-12-04 05:33:39 +0000

    Part 2

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -41,8 +41,40 @@ fn part1(input: Vec<Vec<Spot>>) -> u64 {
     count
 }
 
-fn part2(input: impl Iterator<Item = Vec<u8>>) -> u64 {
-    0
+fn part2(mut grid: Vec<Vec<Spot>>) -> i32 {
+    let mut count = 0;
+    loop {
+        let last_removed = try_remove(&mut grid);
+        count += last_removed;
+        if last_removed == 0 {
+            break;
+        }
+    }
+    
+    count
+}
+
+fn try_remove(grid: &mut Vec<Vec<Spot>>) -> i32 {
+    let mut count = 0;
+    for x in 0..grid.len() {
+        for y in 0..grid[x].len() {
+            if grid[x][y] != Spot::Roll {
+                continue;
+            }
+
+            let adj_roll_count = (-1..=1).flat_map(|dx| (-1..=1).map(move |dy| (dx, dy)))
+                .flat_map(|(dx, dy)| TryInto::<usize>::try_into((x as i32 + dx)).and_then(|nx| (y as i32 + dy).try_into().and_then(|ny: usize| Ok((nx, ny)))))
+                .flat_map(|(nx, ny)| grid.get(nx).and_then(|nrow| nrow.get(ny)))
+                .filter(|v| **v == Spot::Roll)
+                .count();
+            
+            if adj_roll_count <= 4 {
+                grid[x][y] = Spot::Empty;
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -62,15 +94,13 @@ mod tests {
         assert_eq!(result, epxected);
     }
 
-    /*
-    #[test_case("input/sample_input.txt", 3121910778619)]
-    #[test_case("input/input.txt", 170997883706617)]
-    fn test_part2(input_file: &str, epxected: u64) {
+    #[test_case("input/sample_input.txt", 43)]
+    #[test_case("input/input.txt", 8727)]
+    fn test_part2(input_file: &str, epxected: i32) {
         let input_str = fs::read_to_string(input_file).unwrap();
         let input = parse_input(&input_str);
         let result = part2(input);
 
         assert_eq!(result, epxected);
     }
-    */
 }
