341fe96448507a1a7412bbd48d44d30eebd8e08d
yz <4067126+yzheng453@users.noreply.github.com>
2025-12-05 08:56:25 +0000

    Day 5

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -2,49 +2,67 @@ use std::usize;
 
 use regex::Regex;
 
-#[derive(PartialEq)]
-enum Spot {
-    Roll,
-    Empty
-}
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+struct Interval(i64, i64);
 
-fn parse_input(input_str: &String) -> Vec<Vec<Spot>> {
-    input_str
+fn parse_input(input_str: &String) -> (Vec<Interval>, Vec<i64>) {
+    let re: Regex = Regex::new(r"(\d+)-(\d+)").unwrap();
+    let intervals = input_str
         .split_whitespace()
-        .map(|line| line.as_bytes().iter().map(|b| match *b {
-            b'.' => Spot::Empty,
-            b'@' => Spot::Roll,
-            _ => panic!("Unrecognizeable line {}", line),
-        }).collect())
-        .collect()
+        .take_while(|s| s.contains('-'))
+        .map(|s| {
+            re.captures(s)
+                .map(|captures| {
+                    Interval(
+                        captures.get(1).unwrap().as_str().parse().unwrap(),
+                        captures.get(2).unwrap().as_str().parse().unwrap(),
+                    )
+                })
+                .unwrap()
+        })
+        .collect();
+
+    let ingredients = input_str
+        .split_whitespace()
+        .skip_while(|s| s.contains('-'))
+        .map(|s| s.parse().unwrap())
+        .collect();
+    (intervals, ingredients)
 }
 
-fn shift_coord(c: (usize, usize), d: (i32, i32)) -> Option<(usize, usize)> {
-    let nx: usize = (c.0 as i32 + d.0).try_into().ok()?;
-    let ny: usize = (c.1 as i32 + d.1).try_into().ok()?;
-    Some((nx, ny))
-}
-
-fn try_get(coord: (usize, usize), grid: &Vec<Vec<Spot>>) -> Option<&Spot> {
-    let row = grid.get(coord.0)?;
-    row.get(coord.1)
-}
-
-fn part1(input: Vec<Vec<Spot>>) -> u64 {
-    let mut count = 0;
-    for x in 0..input.len() {
-        for y in 0..input[x].len() {
-            if input[x][y] != Spot::Roll {
-                continue;
+fn compact_intervals(mut input_intervals: Vec<Interval>) -> Vec<Interval> {
+    input_intervals.sort();
+    let mut last = input_intervals[0];
+    let mut result = Vec::new();
+    let mut i = 1;
+    while i < input_intervals.len() {
+        let current = input_intervals[i];
+        if current.0 <= last.1 {
+            if current.1 > last.1 {
+                last.1 = current.1;
             }
+        } else {
+            result.push(last);
+            last = current;
+        }
+        i += 1;
+    }
+    result.push(last);
+    result
+}
 
-            let adj_roll_count = (-1..=1).flat_map(|dx| (-1..=1).map(move |dy| (dx, dy)))
-                .flat_map(|d| shift_coord((x, y), d))
-                .flat_map(|c| try_get(c, &input))
-                .filter(|v| **v == Spot::Roll)
-                .count();
-            
-            if adj_roll_count <= 4 {
+fn part1(input: (Vec<Interval>, Vec<i64>)) -> i64 {
+    let (input_intervals, mut ingredients) = input;
+    let interval = compact_intervals(input_intervals);
+    ingredients.sort();
+    let mut interval_index = 0;
+    let mut count = 0;
+    for ingredient in ingredients {
+        while interval_index < interval.len() && interval[interval_index].1 < ingredient {
+            interval_index += 1;
+        }
+        if let Some(i) = interval.get(interval_index) {
+            if i.0 <= ingredient {
                 count += 1;
             }
         }
@@ -52,40 +70,10 @@ fn part1(input: Vec<Vec<Spot>>) -> u64 {
     count
 }
 
-fn part2(mut grid: Vec<Vec<Spot>>) -> i32 {
-    let mut count = 0;
-    loop {
-        let last_removed = try_remove(&mut grid);
-        count += last_removed;
-        if last_removed == 0 {
-            break;
-        }
-    }
-    
-    count
-}
-
-fn try_remove(grid: &mut Vec<Vec<Spot>>) -> i32 {
-    let mut count = 0;
-    for x in 0..grid.len() {
-        for y in 0..grid[x].len() {
-            if grid[x][y] != Spot::Roll {
-                continue;
-            }
-
-            let adj_roll_count = (-1..=1).flat_map(|dx| (-1..=1).map(move |dy| (dx, dy)))
-                .flat_map(|d| shift_coord((x, y), d))
-                .flat_map(|c| try_get(c, &grid))
-                .filter(|v| **v == Spot::Roll)
-                .count();
-            
-            if adj_roll_count <= 4 {
-                grid[x][y] = Spot::Empty;
-                count += 1;
-            }
-        }
-    }
-    count
+fn part2(input: (Vec<Interval>, Vec<i64>)) -> i64 {
+    let (input_intervals, _) = input;
+    let interval = compact_intervals(input_intervals);
+    interval.iter().map(|i| i.1 - i.0 + 1).sum()
 }
 
 #[cfg(test)]
@@ -93,11 +81,11 @@ mod tests {
     use std::fs;
     use test_case::test_case;
 
-    use crate::{parse_input, part1, part2};
+    use crate::{parse_input, part1};
 
-    #[test_case("input/sample_input.txt", 13)]
-    #[test_case("input/input.txt", 1424)]
-    fn test_part1(input_file: &str, epxected: u64) {
+    #[test_case("input/sample_input.txt", 3)]
+    #[test_case("input/input.txt", 513)]
+    fn test_part1(input_file: &str, epxected: i64) {
         let input_str = fs::read_to_string(input_file).unwrap();
         let input = parse_input(&input_str);
         let result = part1(input);
@@ -105,12 +93,12 @@ mod tests {
         assert_eq!(result, epxected);
     }
 
-    #[test_case("input/sample_input.txt", 43)]
-    #[test_case("input/input.txt", 8727)]
-    fn test_part2(input_file: &str, epxected: i32) {
+    #[test_case("input/sample_input.txt", 14)]
+    #[test_case("input/input.txt", 339668510830757)]
+    fn test_part2(input_file: &str, epxected: i64) {
         let input_str = fs::read_to_string(input_file).unwrap();
         let input = parse_input(&input_str);
-        let result = part2(input);
+        let result = crate::part2(input);
 
         assert_eq!(result, epxected);
     }
