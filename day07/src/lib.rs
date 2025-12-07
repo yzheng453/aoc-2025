9b8e5a7e49df148d3dee251451bc68148ed72bde
yz <4067126+yzheng453@users.noreply.github.com>
2025-12-07 11:36:48 +0000

    Rewrite to make it less readable XD

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,75 +1,118 @@
-use std::{collections::{BTreeMap, BTreeSet}, usize};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    usize,
+};
 
 use regex::Regex;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum Cell {
-    Start, Splitter, Empty 
+    Start,
+    Splitter,
+    Empty,
 }
 
 fn parse_input(input_str: &String) -> Vec<Vec<Cell>> {
-    input_str.lines()
-        .map(|line| line.as_bytes()
-            .iter().map(|b| match b {
-                b'S' => Cell::Start,
-                b'^' => Cell::Splitter,
-                b'.' => Cell::Empty,
-                _ => panic!("Unknown char {}", b),
-            })
-            .collect()
-    ).collect()
+    input_str
+        .lines()
+        .map(|line| {
+            line.as_bytes()
+                .iter()
+                .map(|b| match b {
+                    b'S' => Cell::Start,
+                    b'^' => Cell::Splitter,
+                    b'.' => Cell::Empty,
+                    _ => panic!("Unknown char {}", b),
+                })
+                .collect()
+        })
+        .collect()
 }
 
 fn part1(input: Vec<Vec<Cell>>) -> i64 {
-    let start = input.iter().enumerate()
-        .flat_map(|(x, row)| row.iter().enumerate().filter(|(_, c)| **c == Cell::Start).map(move |(y, _)| (x, y)))
-        .next().unwrap();
-    let (_, sum) = input.iter().skip(start.0 + 1).fold({
-        let mut current_beam = BTreeSet::new();
-        current_beam.insert(start.1);
-        (current_beam, 0)
-    },
-        |(current_beam, sum), row|{
-        let mut next_beam = BTreeSet::new();        
-        let splits: i64 = current_beam.iter().map(|b| {
-            match row[*b] {
-                Cell::Empty => {next_beam.insert(*b); 0},
-                Cell::Splitter => {
-                    next_beam.insert(b - 1);
-                    next_beam.insert(b + 1);
-                    1
-                } 
-                _ => panic!(),
-            }
-        }).sum();
-        (next_beam, splits + sum)
-    });
+    let start = input
+        .iter()
+        .enumerate()
+        .flat_map(|(x, row)| {
+            row.iter()
+                .enumerate()
+                .filter(|(_, c)| **c == Cell::Start)
+                .map(move |(y, _)| (x, y))
+        })
+        .next()
+        .unwrap();
+    let (_, sum) = input.iter().skip(start.0 + 1).fold(
+        {
+            let mut current_beam = BTreeSet::new();
+            current_beam.insert(start.1);
+            (current_beam, 0)
+        },
+        |(current_beam, sum), row| {
+            let mut next_beam = BTreeSet::new();
+            let splits: i64 = current_beam
+                .iter()
+                .map(|b| match row[*b] {
+                    Cell::Empty => {
+                        next_beam.insert(*b);
+                        0
+                    }
+                    Cell::Splitter => {
+                        next_beam.insert(b - 1);
+                        next_beam.insert(b + 1);
+                        1
+                    }
+                    _ => panic!(),
+                })
+                .sum();
+            (next_beam, splits + sum)
+        },
+    );
     sum
 }
 
 fn part2(input: Vec<Vec<Cell>>) -> i64 {
-    let start = input.iter().enumerate()
-        .flat_map(|(x, row)| row.iter().enumerate().filter(|(_, c)| **c == Cell::Start).map(move |(y, _)| (x, y)))
-        .next().unwrap();
-    let timelines: BTreeMap<usize, i64> = input.iter().skip(start.0 + 1).fold({
-        let mut current_beam = BTreeMap::new();
-        current_beam.insert(start.1, 1);
-        current_beam
-    },
-        |current_beam, row|{
-        let mut next_beam = BTreeMap::new();        
-        current_beam.iter().for_each(|(b, &count)| {
-            match row[*b] {
-                Cell::Empty => {next_beam.entry(*b).and_modify(|v| *v += count).or_insert(count);},
-                Cell::Splitter => {
-                    next_beam.entry(b - 1).and_modify(|v| *v += count).or_insert(count);
-                    next_beam.entry(b + 1).and_modify(|v| *v += count).or_insert(count);
-                } 
-                _ => panic!(),
-            }
-        });
-        next_beam
-    });
+    let timelines: BTreeMap<usize, i64> = input
+        .iter()
+        .fold(
+            None,
+            |current_beam_opt: Option<BTreeMap<usize, i64>>, row| match current_beam_opt {
+                Some(current_beam) => {
+                    let mut next_beam = BTreeMap::new();
+                    current_beam.iter().for_each(|(b, &count)| match row[*b] {
+                        Cell::Empty => {
+                            next_beam
+                                .entry(*b)
+                                .and_modify(|v| *v += count)
+                                .or_insert(count);
+                        }
+                        Cell::Splitter => {
+                            next_beam
+                                .entry(b - 1)
+                                .and_modify(|v| *v += count)
+                                .or_insert(count);
+                            next_beam
+                                .entry(b + 1)
+                                .and_modify(|v| *v += count)
+                                .or_insert(count);
+                        }
+                        Cell::Start => panic!(),
+                    });
+                    Some(next_beam)
+                }
+                None => row
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, c)| **c == Cell::Start)
+                    .next()
+                    .map(|(column_number, _)| {
+                        let mut current_beam = BTreeMap::new();
+                        current_beam.insert(column_number, 1);
+                        current_beam
+                    }),
+            },
+        )
+        .unwrap();
+
     timelines.values().sum()
 }
 
@@ -78,7 +121,7 @@ mod tests {
     use std::fs;
     use test_case::test_case;
 
-    use crate::{parse_input};
+    use crate::parse_input;
 
     #[test_case("input/sample_input.txt", 21)]
     #[test_case("input/input.txt", 1687)]
